3aab06c554a6411e9786e15bd4094aa559f74aa3
lena beqai <lenabeqai@gmail.com>
2025-01-21 21:56:37 +0300

    Initial commit

---
+++ eCommerceAdmin/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  totalProducts: number = 0;
+  totalCategories: number = 0;
+  totalOrders: number = 0;
+  totalUsers: number = 0;
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.loadDashboardStats();
+  }
+
+  loadDashboardStats(): void {
+    this.dashboardService.getDashboardData().subscribe((data) => {
+      this.totalProducts = data.totalProducts;
+      this.totalCategories = data.totalCategories;
+      this.totalOrders = data.totalOrders;
+      this.totalUsers = data.totalUsers;
+    });
+  }
+}
